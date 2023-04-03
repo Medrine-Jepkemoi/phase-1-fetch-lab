8079c6dd14473ba57c13e2560f4108e4ddb71f3b
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-04-03 22:31:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch('http://localhost:3000/books')
-  .then(res => res.json())
-  .then (data => renderBooks(data))
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((json) => renderBooks(json));
 
   // return fetch()
-
-  
 }
- 
+
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
-    console.log('before')
+    console.log("before");
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
-  console.log('after')
+  console.log("after");
 });
